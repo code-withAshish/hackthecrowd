bd82c1433c59a76c1c14efad4d282374acc14c85
code-withAshish <abhushan064@gmail.com>
2024-11-12 01:50:02 +0530

    testing #6

--- app/util.ts
+++ app/util.ts
@@ -2,7 +2,7 @@ export default function getApiUrl() {
     const environment = process.env.CONTEXT!;
 
     if (environment === "production") {
-        return `https://${process.env.URL!}`;
+        return process.env.URL!;
     } else {
         return process.env.NEXT_PUBLIC_API_URL!;
     }
